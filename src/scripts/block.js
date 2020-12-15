43e1e544a252d49a5837b1ee219ce0b85f96f288
Furkan <fgundfb@gmail.com>
2020-12-15 14:45:06 +0100

    added new block picture and some basic collision (collision working only for "small" numbers, fixing soon)

--- src/scripts/block.js
+++ src/scripts/block.js
@@ -11,6 +11,23 @@ class Block{
         image(blockImg, this.x, this.y, this.w, this.w);
     }
 
+    collide(collidingBlock){
+        return !(this.x + this.w < collidingBlock.x || collidingBlock.x + collidingBlock.w < this.x)
+    }
+
+    hit(collidingBlock){
+        var sumMass = this.m + collidingBlock.m;
+        return ((this.m - collidingBlock.m)/sumMass)*this.v + ((2*collidingBlock.m)/sumMass)*collidingBlock.v;
+    }
+
+    wall(){
+        return (this.x <= 0)
+    }
+
+    reverse(){
+        this.v *= -1; 
+    }
+
     move(){
         this.x += this.v;
     }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,23 +4,40 @@ let blockImg;
 let block1;
 let block2;
 
-function preload(){
-    blockImg = loadImage('/src/img/block.png');    
+let counter = 0;
+
+function preload() {
+  blockImg = loadImage('/src/img/block.png');
 }
 
 function setup() {
-    createCanvas(windowWidth, 250);
-    //           x-Pos. Breite Masse Geschw.
-    block1 = new Block(100, 50, 1, 0);
-    block2 = new Block(500, 200, 10, -1);
+  createCanvas(windowWidth, 250);
+  //           x-Pos. Breite Masse Geschw.
+  block1 = new Block(100, 100, 1, 0);
+  block2 = new Block(500, 150, 10000, -2);
 }
-  
-  function draw() {
-    background(200);
 
-    block1.move();
-    block2.move();
+function draw() {
+  background(200);
 
-    block1.show();
-    block2.show();
+  if (block1.collide(block2)) {
+    const newV1 = block1.hit(block2);
+    const newV2 = block2.hit(block1);
+    block1.v = newV1;
+    block2.v = newV2;
+    counter++;
   }
+
+  if (block1.wall()){
+    counter++;
+    block1.reverse();
+  }
+
+  block1.move();
+  block2.move();
+
+  block1.show();
+  block2.show();
+
+  print(counter);
+}
